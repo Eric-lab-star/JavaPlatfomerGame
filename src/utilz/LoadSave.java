739cb362625a78579d32108fe4eab6354a5fada5
kimkyungsub <cyon256@icloud.com>
2024-04-20 09:32:25 +0900

    updates

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -1,13 +1,18 @@
 package utilz;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
+import main.Game;
+
 public class LoadSave {
   public static final String PLAYER_ATLAS = "player_sprites.png";
+  public static final String LEVEL_ATALAS = "outside_sprites.png";
+  public static final String LEVEL_ONE_DATA = "level_one_data.png";
   public static BufferedImage GetSpriteAtlas(String atlasName) {
     InputStream is = LoadSave.class.getResourceAsStream("/res/" + atlasName);
     BufferedImage img = null;
@@ -23,5 +28,20 @@ public class LoadSave {
       }
     }
     return img;
+  }
+  public static int[][] GetLevelData() {
+    int[][] lvlData = new int[Game.GAME_HEIGHT][Game.GAME_WIDTH];
+    BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+    for (int j = 0; j < img.getHeight(); j++) {
+      for (int i = 0; i < img.getWidth(); i++) {
+        Color color = new Color(img.getRGB(i, j));
+        int value = color.getRed();
+        if (value >= 48) {
+          value = 0;
+        }
+        lvlData[j][i] = value;
+      }
+    }
+    return lvlData;
   }
 }
