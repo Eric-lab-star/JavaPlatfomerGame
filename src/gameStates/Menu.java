563456aaad66d398dbae3446797ab16d2e91b288
kimkyungsub <cyon256@icloud.com>
2024-05-07 08:32:29 +0900

    implement menu class

--- src/gameStates/Menu.java
+++ src/gameStates/Menu.java
@@ -1,40 +1,88 @@
 package gameStates;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import main.Game;
+import ui.MenuButton;
 
 /**
  * Menu
  */
 public class Menu extends State implements StateMethods {
+  private MenuButton[] buttons = new MenuButton[3];
   public Menu(Game game) {
     super(game);
-    // TODO Auto-generated constructor stub
+    initButtons();
+  }
+
+  private void initButtons() {
+    buttons[0] =
+        new MenuButton(Game.GAME_WIDTH / 2, (int) (150 * Game.SCALE), 0, GameState.PLAYING);
+    buttons[1] =
+        new MenuButton(Game.GAME_WIDTH / 2, (int) (220 * Game.SCALE), 1, GameState.OPTIONS);
+    buttons[2] = new MenuButton(Game.GAME_WIDTH / 2, (int) (290 * Game.SCALE), 2, GameState.QUIT);
   }
 
   @Override
-  public void update() {}
+  public void update() {
+    for (MenuButton menuButton : buttons) {
+      menuButton.update();
+    }
+  }
 
   @Override
   public void draw(Graphics g) {
-    g.setColor(Color.BLACK);
-    g.drawString("MENU", Game.GAME_WIDTH / 2, 200);
+    for (MenuButton menuButton : buttons) {
+      menuButton.draw(g);
+    }
   }
 
   @Override
   public void mouseClicked(MouseEvent e) {}
 
   @Override
-  public void mousePressed(MouseEvent e) {}
+  public void mousePressed(MouseEvent e) {
+    for (MenuButton menuButton : buttons) {
+      if (isIn(e, menuButton)) {
+        menuButton.setMousePressed(true);
+        break;
+      }
+    }
+  }
 
   @Override
-  public void mouseReleased(MouseEvent e) {}
+  public void mouseReleased(MouseEvent e) {
+    for (MenuButton menuButton : buttons) {
+      if (isIn(e, menuButton)) {
+        if (menuButton.isMousePressed()) {
+          menuButton.applyGameState();
+          break;
+        }
+      }
+    }
+    resetButtons();
+  }
+
+  private void resetButtons() {
+    for (MenuButton menuButton : buttons) {
+      menuButton.resetBools();
+    }
+  }
 
   @Override
-  public void mouseMoved(MouseEvent e) {}
+  public void mouseMoved(MouseEvent e) {
+    for (MenuButton menuButton : buttons) {
+      menuButton.setMouseOver(false);
+    }
+
+    for (MenuButton menuButton : buttons) {
+      if (isIn(e, menuButton)) {
+        menuButton.setMouseOver(true);
+        break;
+      }
+    }
+  }
 
   @Override
   public void keyPressed(KeyEvent e) {
