4c9e5ed3010158884cc164d719959ea52cf44a9e
kimkyungsub <cyon256@icloud.com>
2024-05-07 07:46:56 +0900

    button class

--- src/ui/MenuButton.java
+++ src/ui/MenuButton.java
@@ -4,6 +4,7 @@ import static utilz.Constants.UI.Buttons.*;
 
 import gameStates.GameState;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import utilz.LoadSave;
 
@@ -12,16 +13,24 @@ import utilz.LoadSave;
  */
 
 public class MenuButton {
-  private int xPos, yPos, rowIndex;
-  private GameState gameState;
+  private int xPos, yPos, rowIndex, index;
+  private int xOffsetCenter = B_WIDTH / 2;
+  private GameState state;
   private BufferedImage[] imgs;
+  private boolean mouseOver, mouseClick;
+  private Rectangle bounds;
 
   public MenuButton(int xPos, int rowIndex, GameState gameState, int yPos) {
     this.xPos = xPos;
     this.rowIndex = rowIndex;
-    this.gameState = gameState;
+    this.state = gameState;
     this.yPos = yPos;
     loadImgs();
+    initBounds();
+  }
+
+  private void initBounds() {
+    bounds = new Rectangle(xPos - xOffsetCenter, yPos, B_WIDTH, B_HEIGHT);
   }
 
   private void loadImgs() {
@@ -33,6 +42,40 @@ public class MenuButton {
       imgs[i] = tmp;
     }
   }
-  public void draw(Graphics g) {}
-  public void update() {}
+  public void draw(Graphics g) {
+    g.drawImage(imgs[index], xPos - xOffsetCenter, yPos, B_WIDTH, B_HEIGHT, null);
+  }
+  public void update() {
+    index = 0;
+    if (mouseOver) {
+      index = 1;
+    }
+
+    if (mouseClick) {
+      index = 2;
+    }
+  }
+
+  public boolean isMouseClick() {
+    return mouseClick;
+  }
+
+  public void setMouseClick(boolean mouseClick) {
+    this.mouseClick = mouseClick;
+  }
+
+  public boolean isMouseOver() {
+    return mouseOver;
+  }
+
+  public void setMouseOver(boolean mouseOver) {
+    this.mouseOver = mouseOver;
+  }
+  public void applyGameState() {
+    GameState.state = state;
+  }
+  public void resetBools() {
+    mouseOver = false;
+    mouseClick = false;
+  }
 }
